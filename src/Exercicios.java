615e16de4147fa7f1e4166b8dd949281ed8baaa9
GabrielaAmbos <gabrielaambos15@gmail.com>
2020-05-19 20:53:10 -0300

    Adicionando os arquivos iniciais

---
+++ src/Exercicios.java
@@ -0,0 +1,23 @@
+
+public class Exercicios {
+
+	/**
+	 * 2. Implemente, em uma classe qualquer, um método que recebe duas filas q1, e q2 de
+	 * inteiros e transfere os elementos da segunda para a primeira de modo que eles fiquem
+	 * na frente dos originais. Exemplo: supondo q1 = {1, 2, 3, 4} e q2 = {5, 6, 7, 8}, após a
+	 * chamada do método devemos ter q1 = {5, 6, 7, 8, 1, 2, 3, 4} e q2 = {}. Dica: use uma fila
+	 * auxiliar.
+	 */
+	public void prependQueue(Queue<Integer> q1, Queue<Integer> q2) {
+		
+	}
+	
+	/**
+	 * 3. Implemente, em uma classe qualquer, um método que remove todas as ocorrências de
+	 * um determinado elemento em uma fila de caracteres. 
+	 */
+	public void exterminateFromQueue(Queue<Character> q, char element) {
+		
+	}
+	
+}
